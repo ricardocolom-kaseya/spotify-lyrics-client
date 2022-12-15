f5c574be5619402492eb733cb2bd13e3b202a4a7
Ricardo Colom <ricardo.colom@kaseya.com>
2022-12-15 16:51:03 -0500

    first commit

---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import { Box, Button, VStack, Text, Heading } from '@chakra-ui/react'
+import React from 'react'
+import { generateRandomString } from './common'
+
+const client_id = '14b9506bc8f84bf698641d4e7033e7fe'
+const redirect_uri = 'http://localhost:3000/callback'
+const state = generateRandomString(16)
+const scope = 'user-read-playback-state%20user-read-currently-playing%20user-read-playback-position'
+
+var url = 'https://accounts.spotify.com/authorize';
+url += '?client_id=' + client_id;
+url += '&response_type=code';
+url += '&redirect_uri=' + redirect_uri;
+url += '&scope=' + scope;
+url += '&state=' + state;
+
+function onLoginClick() {
+    console.log(url)
+    const spotifyLoginWindow = window.open(url)
+}
+
+export default function Login() {
+    return (
+        <Box w="100vw" h="100vh" bg="red">
+            <VStack w="100%" h="100%" bg="gray.800" justify="center" align="center" spacing="8">
+                <Heading fontSize="3xl" color="white">Spotify Lyrics View</Heading>
+                <Button colorScheme="whatsapp" onClick={() => onLoginClick()}>
+                    Log in
+                </Button>
+            </VStack>
+        </Box>
+    )
+}
